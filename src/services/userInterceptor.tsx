4fc9aec50e39f0dcba1d090b00c9d7efc62729ba
Alireza <alireza83safarii@gmail.com>
2023-10-08 13:52:19 +0330

    create axios interceptor

---
+++ src/services/userInterceptor.tsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const userAxios = axios.create({
+  baseURL: "/api/v1/user",
+  withCredentials: true,
+});
+
+userAxios.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+userAxios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (
+      error.response.status === 401 &&
+      error?.response?.request?.responseURL !==
+        "http://localhost:3000/api/v1/user/is_authenticated"
+    ) {
+      toast.error("Unauthorized. Please log in.");
+    }
+
+    return Promise.reject(error);
+  }
+);
+export { userAxios };
