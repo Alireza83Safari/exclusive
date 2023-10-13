42e8a8e6fbeffd7cb32db14ec54e0a14f06fa512
Alireza <alireza83safarii@gmail.com>
2023-10-13 12:14:49 +0330

    add products and category and brand routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -7,8 +7,11 @@ import Chechout from "../pages/Chechout";
 import NotFound from "../components/NotFound";
 import Account from "../pages/Account";
 import About from "../pages/About";
-import Contact from "../pages/Contect";
+import Contact from "../pages/Contact";
 import ProductDetails from "../pages/ProductDetails";
+import Products from "../pages/Products";
+import CategoryResult from "../pages/CategoryResult";
+import BrandResult from "../pages/BrandResult";
 
 const routes = [
   { path: "*", element: <NotFound /> },
@@ -22,6 +25,9 @@ const routes = [
   { path: "about", element: <About /> },
   { path: "contact", element: <Contact /> },
   { path: "product/:productId", element: <ProductDetails /> },
+  { path: "products", element: <Products /> },
+  { path: "category/:category", element: <CategoryResult /> },
+  { path: "brand/:brand", element: <BrandResult /> },
 ];
 
 export default routes;
