209d2e649a90747c58123011e60409185c4382fb
Alireza <alireza83safarii@gmail.com>
2023-10-13 12:12:29 +0330

    added Adrress feature and discount code and order

--- src/pages/Chechout.tsx
+++ src/pages/Chechout.tsx
@@ -1,84 +1,256 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addOrder, getOrderUser } from "../Redux/Store/order";
+import { rootState } from "../Redux/Store";
+import { addAddress, getAddresses } from "../Redux/Store/address";
+import { addressType } from "../types/Address.type";
+import { addressSlice } from "../Redux/Store/address";
+import { codeDiscountValidate } from "../Redux/Store/discount";
+import toast from "react-hot-toast";
 
 function Chechout() {
+  const [copunValue, setCopunValue] = useState<string>("");
+  const [chooseAddress, setChooseAddress] = useState<string>("");
+  const [newAddress, setNewAddress] = useState<addressType>({
+    address: "",
+    firstName: "",
+    lastName: "",
+    nationalCode: "",
+    phoneNumber: "",
+    plaque: null,
+    postalCode: "",
+  });
+
+  const dispatch = useDispatch();
+  const { userOrders, ordersPrice } = useSelector(
+    (state: rootState) => state.order
+  );
+  const { addressError, addresses } = useSelector(
+    (state: rootState) => state.address
+  );
+
+  // get empty error message when focus on input
+  const getEmptyError = () => {
+    const errorValue = Object?.values(addressError);
+    const errorHaveLength = errorValue?.some((error) => error.length >= 1);
+
+    if (errorHaveLength) {
+      dispatch(addressSlice.actions.setAddressError(""));
+    }
+  };
+
+  // check if inputs havent value get disabled button
+  const btnDisabled = useMemo(() => {
+    const newAddresValue = Object.values(newAddress);
+    return newAddresValue.some((item) => item.length < 4);
+  }, [newAddress]);
+
+  const addNewAddress = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    dispatch(addAddress(newAddress) as any);
+  };
+
+  useEffect(() => {
+    dispatch(getOrderUser() as any);
+    dispatch(getAddresses() as any);
+  }, []);
+
+  const setAddreInputValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    if (name == "plaque") {
+      setNewAddress({ ...newAddress, [name]: Number(value) });
+    } else {
+      setNewAddress({ ...newAddress, [name]: value });
+    }
+  };
+
+  const addCopounHandler = () => {
+    dispatch(codeDiscountValidate(copunValue) as any);
+  };
+  const addOrderHandler = () => {
+    if (!chooseAddress) {
+      toast.error("please choose address");
+    } else {
+      dispatch(addOrder(chooseAddress) as any);
+    }
+  };
+
   return (
     <section className="max-w-[1170px] mx-auto my-10 relative sm:px-5 px-2">
       <div className="grid md:grid-cols-2 md:mt-20 mt-10">
-        <form className="lg:pr-28 md:pr-8">
-          <h1 className="text-4xl">Billing Details</h1>
-          <div className="mt-5">
-            <label htmlFor="" className="text-[#999999]">
-              First Name*
-            </label>
-            <input type="text" className="bg-gray w-full py-2 mt-2" w-full />
-          </div>
-          <div className="mt-5">
-            <label htmlFor="" className="text-[#999999]">
-              Company Name
-            </label>
-            <input type="text" className="bg-gray w-full py-2 mt-2" w-full />
-          </div>
-          <div className="mt-5">
-            <label htmlFor="" className="text-[#999999]">
-              Street Address*
-            </label>
-            <input type="text" className="bg-gray w-full py-2 mt-2" w-full />
-          </div>
-          <div className="mt-5">
-            <label htmlFor="" className="text-[#999999]">
-              Apartment, floor, etc. (optional)
-            </label>
-            <input type="text" className="bg-gray w-full py-2 mt-2" w-full />
-          </div>
-          <div className="mt-5">
-            <label htmlFor="" className="text-[#999999]">
-              Town/City*
-            </label>
-            <input type="text" className="bg-gray w-full py-2 mt-2" w-full />
-          </div>
-          <div className="mt-5">
-            <label htmlFor="" className="text-[#999999]">
-              Phone Number*
-            </label>
-            <input type="text" className="bg-gray w-full py-2 mt-2" w-full />
-          </div>
-          <div className="mt-5">
-            <label htmlFor="" className="text-[#999999]">
-              Email Address*
-            </label>
-            <input type="text" className="bg-gray w-full py-2 mt-2" w-full />
-          </div>
-          <div className="flex mt-5">
-            <input
-              type="checkbox"
-              name=""
-              id=""
-              className="checked:text-red mr-2"
-            />
-            <p>Save this information for faster check-out next time</p>
-          </div>
-        </form>
-
+        <div>
+          {addresses.length ? (
+            addresses.map((address) => (
+              <div
+                className={`grid grid-cols-2 mb-5 border border-borderColor p-3 rounded-md hover:bg-gray duration-300 sm:text-base text-sm ${
+                  address.id === chooseAddress && `bg-gray`
+                }`}
+                key={address.id}
+                onClick={() => setChooseAddress(address.id)}
+              >
+                {Object.entries(address).map(
+                  ([key, value], innerIndex, array) =>
+                    key !== "id" &&
+                    key !== "createdAt" &&
+                    key !== "updatedAt" && (
+                      <React.Fragment key={key}>
+                        <div
+                          className={
+                            innerIndex === array.length - 1
+                              ? ""
+                              : "border-b border-gray py-4"
+                          }
+                        >
+                          {key}
+                        </div>
+                        <div
+                          className={
+                            innerIndex === array.length - 1
+                              ? ""
+                              : "border-b border-gray py-4"
+                          }
+                        >
+                          {value}
+                        </div>
+                      </React.Fragment>
+                    )
+                )}
+              </div>
+            ))
+          ) : (
+            <form className="lg:pr-28 md:pr-8">
+              <h1 className="text-4xl">Billing Details</h1>
+              <div className="mt-5">
+                <label htmlFor="" className="text-[#999999]">
+                  First Name<span className="text-red text-lg">*</span>
+                </label>
+                <input
+                  type="text"
+                  className="bg-gray w-full p-2 mt-2 outline-none"
+                  name="firstName"
+                  value={newAddress?.firstName}
+                  onChange={setAddreInputValue}
+                  onFocus={getEmptyError}
+                />
+                <p className="text-red text-xs">{addressError?.firstName}</p>
+              </div>
+              <div className="mt-5">
+                <label htmlFor="" className="text-[#999999]">
+                  Last Name <span className="text-red text-lg">*</span>
+                </label>
+                <input
+                  type="text"
+                  className="bg-gray w-full p-2 mt-2 outline-none"
+                  name="lastName"
+                  value={newAddress?.lastName}
+                  onChange={setAddreInputValue}
+                  onFocus={getEmptyError}
+                />
+                <p className="text-red text-xs">{addressError?.lastName}</p>
+              </div>
+              <div className="mt-5">
+                <label htmlFor="" className="text-[#999999]">
+                  National Code<span className="text-red text-lg">*</span>
+                </label>
+                <input
+                  type="number"
+                  className="bg-gray w-full p-2 mt-2 outline-none"
+                  name="nationalCode"
+                  value={newAddress?.nationalCode}
+                  onChange={setAddreInputValue}
+                  onFocus={getEmptyError}
+                />
+                <p className="text-red text-xs">{addressError?.nationalCode}</p>
+              </div>
+              <div className="mt-5">
+                <label htmlFor="" className="text-[#999999]">
+                  Phone Number<span className="text-red text-lg">*</span>
+                </label>
+                <input
+                  type="number"
+                  className="bg-gray w-full p-2 mt-2 outline-none"
+                  name="phoneNumber"
+                  value={newAddress?.phoneNumber}
+                  onChange={setAddreInputValue}
+                  onFocus={getEmptyError}
+                />
+                <p className="text-red text-xs">{addressError?.phoneNumber}</p>
+              </div>
+              <div className="mt-5">
+                <label htmlFor="" className="text-[#999999]">
+                  plaque<span className="text-red text-lg">*</span>
+                </label>
+                <input
+                  type="number"
+                  className="bg-gray w-full p-2 mt-2 outline-none"
+                  name="plaque"
+                  value={newAddress?.plaque}
+                  onChange={setAddreInputValue}
+                  onFocus={getEmptyError}
+                />
+                <p className="text-red text-xs">{addressError?.plaque}</p>
+              </div>
+              <div className="mt-5">
+                <label htmlFor="" className="text-[#999999]">
+                  Postal Code<span className="text-red text-lg">*</span>
+                </label>
+                <input
+                  type="text"
+                  className="bg-gray w-full p-2 mt-2 outline-none"
+                  name="postalCode"
+                  value={newAddress?.postalCode}
+                  onChange={setAddreInputValue}
+                  onFocus={getEmptyError}
+                />
+                <p className="text-red text-xs">{addressError?.postalCode}</p>
+              </div>
+              <div className="mt-5">
+                <label htmlFor="" className="text-[#999999]">
+                  Address<span className="text-red text-lg">*</span>
+                </label>
+                <textarea
+                  name="address"
+                  rows="4"
+                  className="bg-gray w-full p-2 mt-2 outline-none"
+                  value={newAddress?.address}
+                  onChange={setAddreInputValue}
+                  onFocus={getEmptyError}
+                ></textarea>
+                <p className="text-red text-xs">{addressError?.address}</p>
+              </div>
+              <div className="flex mt-5">
+                <input type="checkbox" className="checked:text-red mr-2" />
+                <p>Save this information for faster check-out next time</p>
+              </div>
+              <button
+                className=" bg-red py-2 px-10 text-white mt-4 disabled:bg-gray disabled:text-red"
+                disabled={btnDisabled}
+                onClick={addNewAddress}
+              >
+                Add Address
+              </button>
+            </form>
+          )}
+        </div>
         <div className="lg:pl-28 md:pl-8 md:mt-0 mt-10">
-          <div className="flex items-center justify-between my-7">
-            <div className="flex items-center">
-              <img src="/images/product-1.png" className="w-10 mr-4" />
-              <p>LCD Monitor</p>
+          {userOrders.map((order) => (
+            <div className="flex items-center justify-between my-7">
+              <div className="flex items-center">
+                <img
+                  src={`http://127.0.0.1:6060/${order.fileUrl}`}
+                  className="w-10 mr-4"
+                />
+                <p>{order.productName}</p>
+              </div>
+              <p>${order.price}</p>
             </div>
-            <p>$650</p>
-          </div>
-
-          <div className="flex items-center justify-between my-10">
-            <div className="flex items-center">
-              <img src="/images/monitor.png" className="w-10 mr-4" />
-              <p>H1 Gamepad</p>
-            </div>
-            <p>$1100</p>
-          </div>
+          ))}
 
           <div className="flex justify-between py-4 border-b border-borderColor">
             <p>Subtotal:</p>
-            <p>$1750</p>
+            <p>${ordersPrice}</p>
           </div>
           <div className="flex justify-between py-4 border-b border-borderColor">
             <p>Shipping:</p>
@@ -86,18 +258,25 @@ function Chechout() {
           </div>
           <div className="flex justify-between py-4">
             <p>Total:</p>
-            <p>$1750</p>
+            <p>${ordersPrice}</p>
           </div>
 
           <div className="flex justify-between items-center mt-5">
             <div className="flex">
-              <input type="radio" name="Bank" id="" className="mr-3" />
+              <input
+                type="radio"
+                name="Bank"
+                id="yourRadioId"
+                className="mr-3"
+                checked
+              />
+
               <p>Bank</p>
             </div>
-            <img src="/images/bank.png" alt="" className="h-8" />
+            <img src="/images/bank.png" className="h-8" />
           </div>
           <div className="flex mt-5">
-            <input type="radio" name="Bank" id="" className="mr-3" />
+            <input type="radio" name="Bank" className="mr-3" />
             <p>Cash on delivery</p>
           </div>
 
@@ -105,13 +284,21 @@ function Chechout() {
             <input
               type="text"
               placeholder="Coupon Code"
-              className="border md:py-3 py-2 col-span-8 pl-4"
+              className="border md:py-3 py-2 col-span-8 pl-4 outline-none"
+              onChange={(e) => setCopunValue(e.target.value)}
             />
-            <button className="bg-red text-white md:py-3 py-2 col-span-4 ml-4 md:text-base text-sm">
+            <button
+              className="bg-red text-white md:py-3 py-2 col-span-4 ml-4 md:text-base text-sm"
+              onClick={addCopounHandler}
+            >
               Apply Coupon
             </button>
           </div>
-          <button className="bg-red text-white md:py-3 py-2 px-5 mt-5 md:text-base text-sm">
+          <button
+            className="bg-red text-white md:py-3 py-2 px-5 mt-5 md:text-base text-sm disabled:bg-gray disabled:text-red"
+            onClick={addOrderHandler}
+            disabled={userOrders.length < 1}
+          >
             Place Order
           </button>
         </div>
