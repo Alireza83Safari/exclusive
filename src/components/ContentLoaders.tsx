89710f3fe257dac62dccb51c9777e985407ec04f
Alireza <alireza83safarii@gmail.com>
2023-10-13 12:18:24 +0330

    create Content Loader

---
+++ src/components/ContentLoaders.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+type ContextLoaderProps = {
+  width: number;
+  height: number;
+};
+function ContentLoaders({ width, height }: ContextLoaderProps) {
+  return (
+    <ContentLoader
+      speed={2}
+      width={width}
+      height={height}
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
+    >
+      <rect x="0" y="0" rx="6" ry="6" width="100%" height="100%" />
+    </ContentLoader>
+  );
+}
+
+export default ContentLoaders;
