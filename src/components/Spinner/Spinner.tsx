ef40114809e858eacb027ea228ff6bb3c5b1c171
Alireza <alireza83safarii@gmail.com>
2023-10-17 14:22:16 +0330

    use new spinner

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -3,15 +3,8 @@ import "./Spinner.css";
 
 function Spinner() {
   return (
-    <div className="flex justify-center items-center m-auto relative h-screen">
-      <div className="sk-chase">
-        <div className="sk-chase-dot"></div>
-        <div className="sk-chase-dot"></div>
-        <div className="sk-chase-dot"></div>
-        <div className="sk-chase-dot"></div>
-        <div className="sk-chase-dot"></div>
-        <div className="sk-chase-dot"></div>
-      </div>
+    <div className="flex justify-center items-center min-w-full">
+      <span className="loader"></span>
     </div>
   );
 }
