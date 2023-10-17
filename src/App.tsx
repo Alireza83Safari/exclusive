964ce0547f805e43291a70d1fff33df00d9bab59
Alireza <alireza83safarii@gmail.com>
2023-10-17 14:55:10 +0330

    add zero scroll when chnage page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import { useRoutes } from "react-router-dom";
+import { useLocation, useRoutes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./pages/Header";
 import routes from "./routes/routes";
 import { Toaster } from "react-hot-toast";
 import { useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getUserInfos } from "./Redux/Store/auth";
 
 function App() {
   const route = useRoutes(routes);
+  const location = useLocation();
   const dispatch = useDispatch();
+  const [dataFetched, setDataFetched] = useState<boolean>(false);
   useEffect(() => {
-    dispatch(getUserInfos() as any);
-  }, []);
+    if (!dataFetched) {
+      dispatch(getUserInfos() as any);
+      setDataFetched(true);
+    }
+  }, [dataFetched]);
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
   return (
     <div className="App">
       <Header />
