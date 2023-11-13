68c6266399466878981633e1c45b8bb14964bacf
Alireza <alireza83safarii@gmail.com>
2023-11-13 15:47:47 +0330

    add start script

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "https://eshop-bak.iran.liara.run",
+        target: "https://eshop-bak.iran.liara.run/",
         changeOrigin: true,
+        headers: {
+          "liara-Proxy-Target": "liara",
+        },
       },
     },
   },
+  plugins: [react()],
 });
